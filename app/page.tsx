2cc71590661892b01da66f0dd4383e400ca78a03
Calvin Baltazar <calvinbaltazar@Calvins-Mac-mini.local>
2025-08-12 14:28:35 +0800

    fix assset paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-import Timeline from "./components/Timeline";
 import Record from "./components/Record";
 
 const Home = () => {
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Export static HTML for GitHub Pages
+  images: {
+    unoptimized: true, // Needed if using next/image
+  },
+  basePath: '/next-portfolio', // Replace with your repo name
+  assetPrefix: '/next-portfolio/', // Same as basePath
+  eslint: {
+    ignoreDuringBuilds: true, // 🚀 Ignore ESLint errors/warnings during build
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
